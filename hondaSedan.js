387f643f6d848d5a5abab991364f0bbd413de797
michaelangelesz <mmweisphal@outlook.com>
2023-05-19 20:37:00 -0400

    rewrote vehiclejs into hondaSedanjs

---
+++ hondaSedan.js
@@ -0,0 +1,50 @@
+const VehicleModule = require('./vehicleBaseClass').Vehicle
+
+class Car extends VehicleModule {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maxPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 180;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+
+    checkService() {
+        if (this.mileage > 30000) {            
+            this.scheduleService = true
+            return this.scheduleService;                       
+        }
+    }
+
+    start() {
+        if (this.fuel > 0) {            
+            console.log("engine has started.");
+            return this.started = true
+        } else {
+            console.log("no fuel");
+            return this.started = false;
+        }
+    }
+
+    loadPassenger(num) {
+        if (this.passenger < this.maxPassengers) {
+            if ((num + this.passenger) <= this.maxPassengers) {
+                this.passenger = num;
+                return this.passenger;               
+            } else {
+                console.log(this.model + " " + this.make + " not have enough space to take all passengers.");
+            }
+        } else {
+            console.log(this.model + " " + this.make + " is full");
+        }
+    }
+}
+
+let myCar = new Car('Honda', 'white-sedan', '2013', 'white', 195000)
+myCar.start()
+myCar.loadPassenger(5)
+myCar.stop()
+myCar.checkService()
+console.log(myCar)
